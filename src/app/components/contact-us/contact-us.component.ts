ab52d8c5907382e3a1ed1f321ac401477d7a4d1b
Tobiloba03 <tobilobaogunberu@gmail.com>
2025-04-23 14:16:11 +0100

    lateset

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ContactUs } from 'src/app/interfaces/interfaces';
 import { FormService } from 'src/app/services/form.service';
@@ -7,58 +8,81 @@ import { UtilService } from 'src/app/services/util.service';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 
 export class ContactUsComponent implements OnInit {
+  contactForm!: FormGroup;
   subscription: Subscription = new Subscription();
-  ePhone: string = "";
-  eEmail: string = "";
-  eName: string = "";
-  eSubject: string = "";
-  eMessage: string = "";
-  objContactUs:ContactUs = {
-    PhoneNumber : '',
-    EmailAddress: '',
+  eName: string = '';
+  ePhone: string = '';
+  eEmail: string = '';
+  eSubject: string = '';
+  eMessage: string = '';
+  objContactUs: ContactUs = {
     Name: '',
+    PhoneNumber: '',
+    EmailAddress: '',
     Subject: '',
-    Message: ''
-  }; 
+    Message: '',
+  };
   isBusy: boolean = false;
-  constructor(private formservice: FormService, private util: UtilService) { }
+
+  constructor(
+    private formservice: FormService,
+    private util: UtilService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-  }
-
-  submitContactUs()
-  {
-    this.isBusy = true;
-    this.objContactUs = {
-      PhoneNumber : this.ePhone,
-      EmailAddress: this.eEmail,
-      Name: this.eName,
-      Subject: this.eSubject,
-      Message: this.eMessage
-    }
-    
-    this.subscription = this.formservice.SubmitContactUs(this.objContactUs).subscribe(res => 
-    {
-      if(res.response == 'success'){
-        this.clearFields();
-        this.isBusy = false;
-        this.util.snackBarNotification("Submitted successfully");
-        
-      }
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
     });
   }
 
-  clearFields()
-  {
-    this.ePhone = "";
-    this.eEmail = "";
-    this.eName = "";
-    this.eSubject = "";
-    this.eMessage = "";
+  submitContactUs() {
+    this.isBusy = true;
+
+    const formValues = this.contactForm.value;
+
+    const objContactUs: ContactUs = {
+      Name: formValues.name,
+      PhoneNumber: formValues.phoneNumber,
+      EmailAddress: formValues.emailAddress,
+      Subject: formValues.subject,
+      Message: formValues.message,
+    };
+
+    this.subscription = this.formservice
+      .SubmitContactUs(objContactUs)
+      .subscribe(
+        (res) => {
+          if (res.response === 'success') {
+            this.contactForm.reset();
+            this.util.snackBarNotification(
+              'Submitted successfully, we will reach out to you soon.'
+            );
+          }
+          this.isBusy = false;
+        },
+        (err: any) => {
+          this.isBusy = false;
+          return this.util.snackBarNotification(
+            'Something went wrong, please check your internet connection'
+          );
+        }
+      );
   }
 
+  clearFields() {
+    this.eName = '';
+    this.ePhone = '';
+    this.eEmail = '';
+    this.eSubject = '';
+    this.eMessage = '';
+  }
 }
