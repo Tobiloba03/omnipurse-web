19b68d6c5cd3c464cc5c3af03da3eb183f46deb1
Tobiloba03 <tobilobaogunberu@gmail.com>
2025-06-20 20:20:03 +0100

    labubu

---
+++ src/app/modules/services/ai-and-iot-innovations/ai-and-iot-innovations.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-and-iot-innovations',
+  templateUrl: './ai-and-iot-innovations.component.html',
+  styleUrls: ['./ai-and-iot-innovations.component.css'],
+})
+export class AiandIoTInnovationsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -5,6 +5,7 @@ import { ServicesRoutingModule } from './services-routing.module';
 import { SoftwareSolutionsComponent } from './software-solutions/software-solutions';
 import { BusinessDevelopmentComponent } from './business-development/business-development.component';
 import { ServicesOverviewComponent } from './services-overview/services-overview.component';
+import { AiandIoTInnovationsComponent } from './ai-and-iot-innovations/ai-and-iot-innovations.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     BusinessDevelopmentComponent,
     SoftwareSolutionsComponent,
     ServicesOverviewComponent,
+    AiandIoTInnovationsComponent,
   ],
   imports: [CommonModule, ServicesRoutingModule, SharedModule],
 })
